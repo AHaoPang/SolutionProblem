af24eb3627d46037f6d969a1e907788e2c68f2b3
pangyanhao <pangyanhao@beisen.com>
2019-02-14 23:13:13 +0800

    22题处理

---
+++ ProblemSolutions/Problem022.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolutions
+{
+    public class Problem022 : IProblem
+    {
+        public void RunProblem()
+        {
+            var temp = GenerateParenthesis(3);
+        }
+
+        public IList<string> GenerateParenthesis(int n)
+        {
+            IList<string> forReturn = new List<string>();
+            Gen(forReturn, n, 0, 0, "");
+            return forReturn;
+        }
+
+        private void Gen(IList<string> forReturn,int total,int left,int rigth,string str)
+        {
+            if(left == total && rigth == total)
+            {
+                forReturn.Add(str);
+                return;
+            }
+
+            if(left < total)
+                Gen(forReturn, total, left + 1, rigth, str + "(");
+
+            if (rigth < left)
+                Gen(forReturn, total, left, rigth + 1, str + ")");
+        }
+    }
+}
