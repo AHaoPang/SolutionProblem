9a023b9ed21617c8a61fb3309d133703a2d6f322
pangyanhao <pangyanhao@beisen.com>
2019-02-26 08:05:33 +0800

    231

---
+++ ProblemSolutions/Problem231.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolutions
+{
+    public class Problem231 : IProblem
+    {
+        public void RunProblem()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsPowerOfTwo(int n)
+        {
+            return n != 0 && (n & (n - 1)) == 0;
+        }
+    }
+}
