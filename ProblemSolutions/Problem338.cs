98a7363c54b8a7252fcc0091dfef69a1e8edbd9f
pangyanhao <pangyanhao@beisen.com>
2019-02-26 08:11:14 +0800

    338

---
+++ ProblemSolutions/Problem338.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolutions
+{
+    public class Problem338 : IProblem
+    {
+        public void RunProblem()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int[] CountBits(int num)
+        {
+            int[] forReturn = new int[num + 1];
+
+            forReturn[0] = 0;
+            for (int i = 1; i <= num; i++)
+                forReturn[i] = forReturn[i & (i - 1)] + 1;
+
+            return forReturn;
+        }
+    }
+}
