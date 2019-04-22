f77c375fdae48fd8f6802d3efa1a5a9d7255d548
pangyanhao <pangyanhao@beisen.com>
2019-04-22 08:24:06 +0800

    206 092

---
+++ ProblemSolutions/Problem092.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolutions
+{
+    public class Problem092 : IProblem
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x) { val = x; }
+        }
+
+        public void RunProblem()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ListNode ReverseBetween(ListNode head, int m, int n)
+        {
+            /*
+             * 链表的部分反转实现
+             * 必须的指针有3个：
+             * pre：截断前的一个位置；
+             * head：阶段后的一个位置；
+             * next：本次要操作的位置；
+             * 
+             * 时间复杂度：O(n);
+             * 空间复杂度：O(1);
+             */
+
+            ListNode dummy = new ListNode(0);
+            dummy.next = head;
+
+            //定位到开始的位置
+            ListNode pre = dummy;
+            for(int i = 1; i < m; i++)
+            {
+                head = head.next;
+                pre = pre.next;
+            }
+
+            //循环操作节点
+            ListNode next = null;
+            for(int j = m;j < n; j++)
+            {
+                next = head.next;
+                head.next = next.next;
+                next.next = pre.next;
+                pre.next = next;
+            }
+
+            return dummy.next;
+        }
+    }
+}
---
+++ ProblemSolutions/Problem206.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolutions
+{
+    public class Problem206 : IProblem
+    {
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x) { val = x; }
+        }
+
+        public void RunProblem()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ListNode ReverseList(ListNode head)
+        {
+            /*
+             * 链表反转，方法二：递归法
+             * 将业务操作看作是重复的解决子问题：已经反转好的链表，再加上当前的结点
+             * 时间复杂度：O(n),即要处理每个链表结点
+             * 空间复杂度：O(n)，即需要递归n次，是一个n层循环
+             */
+
+            //end point
+            if (head == null || head.next == null) return head;//当进入到最末一个节点时，当前节点就是新的头指针了~
+
+            //reverse operate
+            var newHead = ReverseList(head.next);
+            //需要修改当前结点了
+            head.next.next = head;
+            head.next = null;
+
+            //return 
+            return newHead;
+        }
+
+        public ListNode ReverseList2(ListNode head)
+        {
+            /*
+             * 链表反转，方法一：迭代法，即循环的方式
+             * key point：涉及3个指针的责任交替
+             * 时间复杂度：O(n)
+             * 空间复杂度：O(1)
+             * 
+             * 总结：循环过程中，包含两部分操作
+             * 1.链表相关的操作；
+             * 2.指针相关的操作；
+             */
+
+            ListNode newListNode = null;
+            ListNode curListNode = head;
+            ListNode nextListNode = null;
+
+            while(curListNode != null)
+            {
+                nextListNode = curListNode.next;
+
+                //链表相关的操作
+                curListNode.next = newListNode;
+
+                newListNode = curListNode;
+                curListNode = nextListNode;
+            }
+
+            return newListNode;
+        }
+    }
+}
