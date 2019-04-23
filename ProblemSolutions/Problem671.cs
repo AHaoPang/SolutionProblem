f5ec689676f98d1c8366df6c635d0c1e9b2fb6b1
pangyanhao <pangyanhao@beisen.com>
2019-04-24 06:30:16 +0800

    671 679

---
+++ ProblemSolutions/Problem671.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolutions
+{
+    public class Problem671 : IProblem
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int x) { val = x; }
+        }
+
+        public void RunProblem()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int FindSecondMinimumValue(TreeNode root)
+        {
+            /*
+             * 解析思路：利用递归的思想
+             * 此树的特性，明显是个小顶堆
+             * 若是找最小，那么直接就是根节点了，现在要第二个小的，那么就是第一个比根节点大的那个了~
+             */
+
+            return MinimumValue(root, root.val);
+        }
+
+        private int MinimumValue(TreeNode root,int matchValue)
+        {
+            //到尽头了，那就说明没找到比根大的值
+            if (root == null) return -1;
+
+            //首次找到比根大的值，那么就返回，不要再继续查找了
+            if (root.val > matchValue) return root.val;
+
+            //继续往下找
+            int leftValue = MinimumValue(root.left, matchValue);
+            int rightValue = MinimumValue(root.right, matchValue);
+
+            //两个子都有比根大的值，那就取两者间的小的
+            if (leftValue > 0 && rightValue > 0)
+                return Math.Min(leftValue, rightValue);
+
+            //二者仅有一个找到了，或者都没找到，那么返回较大的好了
+            return Math.Max(leftValue, rightValue);
+        }
+
+        public int FindSecondMinimumValue2(TreeNode root)
+        {
+            /*
+             * 解题思路：遍历树的各个结点，搜集数据；排序后输出第二小的数字
+             * 时间复杂度：O(nlogn)
+             * 空间复杂度：O(n)
+             * 
+             * 此种方式，没有很好的利用树自下而上逐渐变小的规律，不太好
+             */
+
+            HashSet<int> nums = new HashSet<int>();
+            Reverse(root, nums);
+
+            var list = nums.OrderBy(i => i).Take(2).ToList();
+            if (list.Count < 2) return -1;
+            else return list[1];
+        }
+
+        private void Reverse(TreeNode root, HashSet<int> nums)
+        {
+            /*
+             * 根->左->右 的循环遍历方式
+             */
+
+            if (root == null) return;
+            nums.Add(root.val);
+
+            Reverse(root.left, nums);
+            Reverse(root.right, nums);
+        }
+    }
+}
---
+++ ProblemSolutions/Problem679.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolutions
+{
+    public class Problem679 : IProblem
+    {
+        public void RunProblem()
+        {
+            var temp = JudgePoint24(new int[] { 4, 1, 8, 7 });
+        }
+
+        public bool JudgePoint24(int[] nums)
+        {
+            /*
+             * 采用回溯的办法，依次遍历所有可能性，知道有一种满足条件
+             * 时间复杂度：可能性是有上限的，所以是 O(1)；
+             * 空间复杂度：O(1)；
+             */
+
+            List<double> numLists = new List<double>(nums.Select(i => (double)i));
+
+            return ReverseSolve(numLists);
+        }
+
+        public bool ReverseSolve(List<double> numLists)
+        {
+            //end point
+            if (numLists.Count == 0) return false;//不太可能出现这种情况
+            if (numLists.Count == 1) return Math.Abs(numLists[0] - 24) < 0.00001;//小于一定的精度，就可以认为是相等的
+
+            //从数组中取出两个数字
+            for (int i = 0; i < numLists.Count; i++)
+            {
+                for (int j = 0; j < numLists.Count; j++)
+                {
+                    if (i == j) continue;
+
+                    //把剩余的数字加到数组中
+                    List<double> newNumLists = new List<double>();
+                    for (int k = 0; k < numLists.Count; k++) if (k != i && k != j) newNumLists.Add(numLists[k]);
+
+                    //开始将取出的数字与运算符组合
+                    for (int l = 0; l < 4; l++)
+                    {
+                        if (l < 2 && j > i) continue;
+                        else if (l == 0) newNumLists.Add(numLists[i] + numLists[j]);
+                        else if (l == 1) newNumLists.Add(numLists[i] * numLists[j]);
+                        else if (l == 2) newNumLists.Add(numLists[i] - numLists[j]);
+                        else
+                        {
+                            if (numLists[j] == 0) continue;
+                            newNumLists.Add(numLists[i] / numLists[j]);
+                        }
+
+                        if (ReverseSolve(newNumLists)) return true;
+                        newNumLists.RemoveAt(newNumLists.Count - 1);
+                    }
+                }
+            }
+
+            return false;//没有任何可能性了，所以就返回false
+        }
+    }
+}
