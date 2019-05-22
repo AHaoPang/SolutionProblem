405d3f70df0eaa3a5c2775c3a59ea0fd6274c8ad
pangyanhao <pangyanhao@beisen.com>
2019-05-23 07:47:44 +0800

    025 895

---
+++ ProblemSolutions/Problem025.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolutions
+{
+    public class Problem025 : IProblem
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x) { val = x; }
+        }
+
+        public void RunProblem()
+        {
+            var n1 = new ListNode(1);
+            var n2 = new ListNode(2);
+            var n3 = new ListNode(3);
+            var n4 = new ListNode(4);
+            var n5 = new ListNode(5);
+
+            n1.next = n2;
+            n2.next = n3;
+            n3.next = n4;
+            n4.next = n5;
+
+            var temp = ReverseKGroup(n1, 2);
+        }
+
+        public ListNode ReverseKGroup(ListNode head, int k)
+        {
+            var headTemp = new ListNode(-1);
+            headTemp.next = head;
+            Recursive(headTemp, k);
+            return headTemp.next;
+        }
+
+        private void Recursive(ListNode preNode, int k)
+        {
+            /*
+             * 反转思路说明：
+             * 1.要明确，反转的是哪个范围里面的结点；
+             * 2.反转的具体过程是：
+             *      2.1 直接指向尾
+             *      2.2 断掉的部分依次遍历，插入到起始的位置
+             *      
+             * 里面使用到了递归的思想，即链表不管有多长，始终反转特定长度的一段，然后不断的重复做相同的事情，直到全部检查一遍并反转完成
+             * 
+             * 实现原则：
+             * 1.明确各个指针的含义和职责，不要去变动
+             * 2.各个指针自身演变的时候，多做思考
+             * 3.深刻的去理解指针的移动，与，结点的移动 的关系
+             * 
+             * 时间复杂度：O(n) 其实是遍历了3次的样子
+             * 空间复杂度：O(1)
+             */
+
+            //定位被反转链表的结束为止
+            var lastNode = preNode;
+            int i = 0;
+            for (; i < k; i++)
+            {
+                if (lastNode.next == null) break;
+
+                lastNode = lastNode.next;
+            }
+            //说明长度达不到反转的标准k，因此结束
+            if (i != k) return;
+
+            //将前驱结点与结束结点一起连到结束的位置
+            var nextNode = preNode.next;
+            preNode.next = lastNode.next;
+
+            //开始遍历断掉的分支部分
+            var stopNode = lastNode.next;
+            while (nextNode != stopNode)
+            {
+                var readyNode = nextNode;
+                nextNode = nextNode.next;
+
+                readyNode.next = preNode.next;
+                preNode.next = readyNode;
+            }
+
+            //移动preNode k 次，递归的去检查一下段
+            for (int j = 0; j < k; j++) preNode = preNode.next;
+            Recursive(preNode, k);
+        }
+    }
+}
---
+++ ProblemSolutions/Problem895.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolutions
+{
+    public class Problem895 : IProblem
+    {
+        public void RunProblem()
+        {
+            throw new NotImplementedException();
+        }
+
+        public class FreqStack
+        {
+            /// <summary>
+            /// 依据输入，来做汇总统计（同一个输入值，汇总到一起）
+            /// </summary>
+            Dictionary<int, int> m_inputCount = new Dictionary<int, int>();
+
+            /// <summary>
+            /// 依据出现的次数，来做汇总统计（同一个出现数量，汇总到一起）
+            /// </summary>
+            Dictionary<int, Stack<int>> m_freqCount = new Dictionary<int, Stack<int>>();
+
+            /// <summary>
+            /// 目前维护的输入中，出现的最大次数
+            /// </summary>
+            int m_maxCount = 0;
+
+            public FreqStack()
+            {
+
+            }
+
+            public void Push(int x)
+            {
+                if (!m_inputCount.ContainsKey(x)) m_inputCount[x] = 0;
+                m_inputCount[x]++;
+
+                int freqAmount = m_inputCount[x];
+
+                m_maxCount = Math.Max(m_maxCount, freqAmount);
+
+                if (!m_freqCount.ContainsKey(freqAmount)) m_freqCount[freqAmount] = new Stack<int>();
+                m_freqCount[freqAmount].Push(x);
+            }
+
+            public int Pop()
+            {
+                var forReturn = m_freqCount[m_maxCount].Pop();
+
+                if (!m_freqCount[m_maxCount].Any()) m_maxCount--;
+
+                m_inputCount[forReturn]--;
+                if (m_inputCount[forReturn] == 0) m_inputCount.Remove(forReturn);
+
+                return forReturn;
+            }
+        }
+    }
+}
