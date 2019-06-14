cccc8e698f8d62e56d954a84b420b3c01da642bb
pangyanhao <pangyanhao@beisen.com>
2019-06-15 07:37:34 +0800

    138

--- ProblemSolutions/Problem138.cs
+++ ProblemSolutions/Problem138.cs
@@ -42,6 +42,153 @@ namespace ProblemSolutions
         public Node CopyRandomList(Node head)
         {
             /*
+             * 复杂链表的拷贝
+             * 思路：
+             *  1.第一步，double原来的链表，依据next做拷贝
+             *  2.第二步，拷贝原来的random
+             *  3.第三步，拆分拷贝链表
+             *  
+             * 时间复杂度：O(n)
+             * 空间复杂度：O(1)，因为并不使用额外的存储空间
+             */
+
+            Node oldNodeHead = new Node();
+            oldNodeHead.next = head;
+
+            Node oldNodeTail = oldNodeHead;
+            while(oldNodeTail.next != null)
+            {
+                oldNodeTail = oldNodeTail.next;
+
+                var newNodeTemp = new Node(oldNodeTail.val, oldNodeTail.next, null);
+                oldNodeTail.next = newNodeTemp;
+
+                oldNodeTail = newNodeTemp;
+            }
+
+            oldNodeTail = oldNodeHead;
+            while(oldNodeTail.next != null)
+            {
+                oldNodeTail = oldNodeTail.next;
+
+                if (oldNodeTail.random != null)
+                    oldNodeTail.next.random = oldNodeTail.random.next;
+
+                oldNodeTail = oldNodeTail.next;
+            }
+
+            Node newNodeHead = new Node();
+            Node newNodeTail = newNodeHead;
+
+            oldNodeTail = oldNodeHead;
+            while(oldNodeTail.next != null)
+            {
+                oldNodeTail = oldNodeTail.next;
+
+                newNodeTail.next = oldNodeTail.next;
+
+                if (oldNodeTail.next != null)
+                    oldNodeTail.next = oldNodeTail.next.next;
+
+                newNodeTail = newNodeTail.next;
+            }
+
+            return newNodeHead.next;
+        }
+
+        public Node CopyRandomList3(Node head)
+        {
+            /*
+             * 复杂链表的拷贝问题
+             * 思路：
+             *  1.把复杂链表看成是一个特殊的二叉树，两个指针分别叫做next和random
+             *  2.复杂链表的拷贝，可以看做是树的遍历过程
+             *  3.按图索骥的感觉，使用递归的方式，思路意外的清晰
+             *  
+             * 时间复杂度：O(n)，所有的节点都是要过一遍的
+             * 空间复杂度：O(n)，要额外的存储新旧节点的映射关系
+             */
+
+            return Recursive(new Dictionary<Node, Node>(), head);
+        }
+
+        private Node Recursive(Dictionary<Node, Node> oldToNewNodeDic, Node oldNode)
+        {
+            if (oldNode == null) return null;
+            if (oldToNewNodeDic.ContainsKey(oldNode)) return oldToNewNodeDic[oldNode];
+
+            Node newNodeTemp = new Node(oldNode.val, null, null);
+            oldToNewNodeDic[oldNode] = newNodeTemp;
+
+            newNodeTemp.next = Recursive(oldToNewNodeDic, oldNode.next);
+            newNodeTemp.random = Recursive(oldToNewNodeDic, oldNode.random);
+
+            return newNodeTemp;
+        }
+
+        public Node CopyRandomList2(Node head)
+        {
+            /*
+             * 复杂链表的拷贝问题
+             * 思路：
+             *  1.用循环的方式，循环主线节点，并把相关的节点一并创建
+             *  
+             * 时间复杂度：O(n)，与链表节点个数有关
+             * 空间复杂度：O(n)，需要建立新旧节点的映射关系
+             */
+
+            Node oldHead = new Node();
+            oldHead.next = head;
+            Node oldHeadTail = oldHead;
+
+            Node newHead = new Node();
+            Node newHeadTail = newHead;
+
+            Dictionary<Node, Node> oldToNewNodeDic = new Dictionary<Node, Node>();
+
+            while (oldHeadTail.next != null)
+            {
+                //原链表指向下一个节点
+                oldHeadTail = oldHeadTail.next;
+
+                //复制主链表节点
+                Node newNodeNext = null;
+                if (oldToNewNodeDic.ContainsKey(oldHeadTail))
+                    newNodeNext = oldToNewNodeDic[oldHeadTail];
+
+                if (newNodeNext == null)
+                {
+                    newNodeNext = new Node(oldHeadTail.val, null, null);
+                    oldToNewNodeDic[oldHeadTail] = newNodeNext;
+                }
+
+                //复制指向的随机节点
+                if (oldHeadTail.random != null)
+                {
+                    Node newNodeRandom = null;
+                    if (oldToNewNodeDic.ContainsKey(oldHeadTail.random))
+                        newNodeRandom = oldToNewNodeDic[oldHeadTail.random];
+
+                    if (newNodeRandom == null)
+                    {
+                        newNodeRandom = new Node(oldHeadTail.random.val, null, null);
+                        oldToNewNodeDic[oldHeadTail.random] = newNodeRandom;
+                    }
+
+                    newNodeNext.random = newNodeRandom;
+                }
+
+                //将新的节点添加到新的链表中
+                newHeadTail.next = newNodeNext;
+                newHeadTail = newHeadTail.next;
+            }
+
+            return newHead.next;
+        }
+
+        public Node CopyRandomList1(Node head)
+        {
+            /*
              * 深度拷贝一个带随机指针的链表
              * 思路：
              *  1.分析下带随机指针的链表，它有且只有一个主链表，然后就是随机的指针了；
