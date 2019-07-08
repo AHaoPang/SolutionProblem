9f4298c9595bf049bd06692a71b948b93b33fc13
pangyanhao <pangyanhao@beisen.com>
2019-07-09 07:44:28 +0800

    326

---
+++ ProblemSolutions/Problem326.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolutions
+{
+    public class Problem326 : IProblem
+    {
+        public void RunProblem()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsPowerOfThree(int n)
+        {
+            /*
+             * 判断n是否是3的次幂
+             * 思路：
+             *  1.将n转换为3进制的数，判断是否只有一个1即可
+             *  
+             * 时间复杂度：O(logn)
+             * 空间复杂度：O(1)
+             */
+
+            if (n < 1) return false;
+
+            while (n % 3 == 0) n /= 3;
+
+            return n == 1;
+        }
+    }
+}
