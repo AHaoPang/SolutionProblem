443198384da501df000d8e48713291993e9f0f1b
pyh <pangyanhao@beisen.com>
2019-08-23 09:13:02 +0800

    274 299

---
+++ ProblemSolutions/Problem274.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolutions
+{
+    public class Problem274 : IProblem
+    {
+        public void RunProblem()
+        {
+            var temp = HIndex(new int[] { 3, 0, 6, 1, 5 });
+            if (temp != 3) throw new Exception();
+        }
+
+        public int HIndex(int[] citations)
+        {
+            /*
+             * h指数求解
+             * 思路：
+             *  1.当平均引用数量高的时候，受限于论文数量；
+             *  2.当论文数量高时，受限于引用数量；
+             *  3.所以，h指数，总是反映出短板的那一面，逻辑上引用次数会远高于论文数量，因此还是对论文数量有要求
+             *  4.给定的论文数量是有限的，那么结果的取值基本也是有限的了
+             *  5.引用次数超过论文数n的论文，也一定超过论文数n-1
+             *  
+             * 时间复杂度：O(1)，遍历2次数组，第一遍写值，第二遍逆序查找满足要求的
+             * 空间复杂度：O(n)，需要一个同样大小的数组来做统计
+             */
+
+            int[] countArray = new int[citations.Length + 1];
+
+            foreach (var citationItem in citations)
+            {
+                if (citationItem > citations.Length)
+                {
+                    countArray[citations.Length]++;
+                    continue;
+                }
+
+                countArray[citationItem]++;
+            }
+
+            int totalNumTemp = 0;
+            for (int i = countArray.Length - 1; i >= 0; i--)
+            {
+                totalNumTemp += countArray[i];
+
+                if (i <= totalNumTemp) return i;
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ ProblemSolutions/Problem299.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolutions
+{
+    public class Problem299 : IProblem
+    {
+        public void RunProblem()
+        {
+            var temp = GetHint("1122", "2211");
+            if (temp != "0A4B") throw new Exception();
+
+            temp = GetHint("1807", "7810");
+            if (temp != "1A3B") throw new Exception();
+
+            temp = GetHint("1123", "0111");
+            if (temp != "1A1B") throw new Exception();
+        }
+
+        public string GetHint(string secret, string guess)
+        {
+            /*
+             * 猜数字游戏
+             * 思路：
+             *  1.我告诉你有几位数，且每一位都是0~9的数字
+             *  2.你来告诉我猜测结果
+             *  3.我在告诉你，哪些位置上的数字是对的，哪些数字是存在的但是位置不对
+             *  
+             *  4.依次遍历秘密数字和猜测数字的每一位
+             *  5.若相同，那么就记录“bull”
+             *  6.若不同，则分别放在秘密数字集合字典，与猜测数字集合里
+             *  
+             * 时间复杂度：O(n) ，遍历数字串2次
+             * 空间复杂度：O(n) ，用额外的空间记录可能性集合
+             */
+
+            int bulls = 0;
+            int cows = 0;
+            Dictionary<char, int> secretChar = new Dictionary<char, int>();
+            List<char> guessChar = new List<char>();
+            for(int i = 0;i < secret.Length; i++)
+            {
+                if(secret[i] == guess[i])
+                {
+                    bulls++;
+                    continue;
+                }
+
+                if (!secretChar.ContainsKey(secret[i])) secretChar[secret[i]] = 0;
+
+                secretChar[secret[i]]++;
+                guessChar.Add(guess[i]);
+            }
+
+            foreach(var guessCharItem in guessChar)
+            {
+                if(secretChar.ContainsKey(guessCharItem) && secretChar[guessCharItem] > 0)
+                {
+                    cows++;
+                    secretChar[guessCharItem]--;
+                }
+            }
+
+            return $"{bulls}A{cows}B";
+        }
+    }
+}
