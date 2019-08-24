d8f0e8383f1b333d2a4e7381ba75c2075670a4b0
pyh <pangyanhao@beisen.com>
2019-08-24 22:09:06 +0800

    创建高版本的Framework

--- NETFramework472Attempt/Program.cs
+++ NETFramework472Attempt/Program.cs
@@ -3,19 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Runtime.CompilerServices;
 
-namespace NETFrameworkAttempt
+namespace NETFramework472Attempt
 {
     class Program
     {
         static void Main(string[] args)
         {
-
-            //(int a, int b) testTuple = (a: 15, b: 20);
-
-            
-
         }
     }
 }
