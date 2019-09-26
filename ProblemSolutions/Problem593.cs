11fd9218bf8aacc4a35cc4de8a29bc3b04ac0231
pyh <pangyanhao@beisen.com>
2019-09-26 08:31:22 +0800

    593 598

---
+++ ProblemSolutions/Problem593.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolutions
+{
+    public class Problem593 : IProblem
+    {
+        public void RunProblem()
+        {
+            var temp = ValidSquare(new int[] { 0, 0 }, new int[] { 1, 1 }, new int[] { 1, 0 }, new int[] { 0, 1 });
+            if (temp != true) throw new Exception();
+        }
+
+        public bool ValidSquare(int[] p1, int[] p2, int[] p3, int[] p4)
+        {
+            /*
+             * 判断平面上给定的4个点，是否构成一个正方形
+             * 思路：
+             *  1. 4个点，可以构成6条边，只要满足4条边（正边）相等，2条边（对角线）相等，即可
+             *  
+             * 时间复杂度：O(1)
+             * 空间复杂度：O(1)
+             */
+
+            var lengthDic = new Dictionary<int, int>();
+            int[][] points = new int[][] { p1, p2, p3, p4 };
+            for (int i = 0; i < points.GetLength(0); i++)
+            {
+                for (int j = i + 1; j < points.GetLength(0); j++)
+                {
+                    var xLength = points[j][0] - points[i][0];
+                    var yLength = points[j][1] - points[i][1];
+                    var sumLength = xLength * xLength + yLength * yLength;
+
+                    if (!lengthDic.ContainsKey(sumLength)) lengthDic[sumLength] = 0;
+                    lengthDic[sumLength]++;
+                }
+            }
+
+            if (lengthDic.Count == 2)
+            {
+                if (lengthDic.First().Value == 2 && lengthDic.Last().Value == 4) return true;
+                if (lengthDic.First().Value == 4 && lengthDic.Last().Value == 2) return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ ProblemSolutions/Problem598.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolutions
+{
+    public class Problem598 : IProblem
+    {
+        public void RunProblem()
+        {
+            var temp = MaxCount(3, 3, new int[][] { new int[] { 2, 2 }, new int[] { 3, 3 } });
+            if (temp != 4) throw new Exception();
+
+            temp = MaxCount(3, 3, new int[][] { });
+            if (temp != 9) throw new Exception();
+        }
+
+        public int MaxCount(int m, int n, int[][] ops)
+        {
+            /*
+             * 范围求和
+             * 思路：
+             *  1. 长为m 宽为n 的一个矩阵，里面按照左上角对齐的方式，放置了多个小的矩形
+             *  2. 那么问题就可以转换为，这些小矩形重合的地方有多大
+             *  3. 也就是求得交集的过程，那么只要找到最短的边就可以了
+             *  
+             * 时间复杂度：O(n)
+             * 空间复杂度：O(1)
+             */
+
+            int minM = m;
+            int minN = n;
+            for(int i = 0;i < ops.GetLength(0); i++)
+            {
+                minM = Math.Min(minM, ops[i][0]);
+                minN = Math.Min(minN, ops[i][1]);
+            }
+
+            return minM * minN;
+        }
+    }
+}
